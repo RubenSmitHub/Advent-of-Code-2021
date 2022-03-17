e1b71472171e2840e8e3cbf3e23c273c3da29ef5
Ruben Smit <ruben_19_92@hotmail.com>
2022-03-17 21:41:51 +0100

    wip

---
+++ Day 3/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day_3
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string Filepath = $@"{Environment.CurrentDirectory}\Data\input.txt";
+      
+      int[] answers = { 0,0,0,0,0,0,0,0,0,0,0,0};
+      int count = 0;
+
+      foreach (string line in File.ReadAllLines(Filepath))
+      {
+        count++;
+        Console.WriteLine($"line: {line}");
+
+        for (int i = 0; i < 12; i++)
+        {
+          string character = line.Substring(i, 1);
+          int value = int.Parse(character);
+          answers[i] += value;
+
+        }
+      }
+
+      Console.WriteLine($"answer {answers.ToString()}");
+
+      BitArray gammaRateBuilder = new BitArray(12);
+      BitArray epsilonRateBuilder = new BitArray(12);
+
+
+      for (int i = 0; i < 12; i++)
+      {
+        gammaRateBuilder.Set(i, answers[i] >= (count / 2));
+        epsilonRateBuilder.Set(i, answers[i] <= (count / 2));
+
+        
+      }
+
+
+      int[] epsilonRate = new int[1];
+      epsilonRateBuilder.CopyTo(epsilonRate, 0);
+
+      int[] gammaRate = new int[1];
+      gammaRateBuilder.CopyTo(gammaRate, 0);
+
+      Console.WriteLine($"epsilon rate: {epsilonRate[0]}");
+      Console.WriteLine($"gamma rate: {gammaRate[0]}");
+
+      Console.WriteLine($"Final answer: {epsilonRate[0] * gammaRate[0]}");
+
+      
+    }
+  }
+}
